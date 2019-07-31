5ae51cba98836a12a091403d6169b13c840c8e3d
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-07-31 17:47:37 +0100

    DATABASE_URL added to app.py; including extra pages

--- flask-sqlalchemy-test-02/app.py
+++ flask-sqlalchemy-test-02/app.py
@@ -1,27 +1,29 @@
 import os
 import psycopg2
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, url_for
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-
-# def connect_db():
-#     return psycopg2.connect(os.environ.get('DATABASE_URL'))
-
-
-@app.before_request
-def before_request():
-    g.db_conn = connect_db()
-    # to be muted
-
+DATABASE_URL = os.environ.get('postgres://fikwczdiymxhwf:73bf42c2c8a15fa59b77e93654b6383e1cf4f85bdf0156818d1cf39a77815f13@ec2-54-243-47-196.compute-1.amazonaws.com:5432/d3uburco4fea1b')
 
 @app.route('/')
 def index():
-    cur = g.db_conn.cursor()
-    cur.execute("SELECT * FROM country;")
-    return render_template('index.html', countries=cur.fetchall())
-    # to be muted
-    
+    return render_template('index.html')
+
+@app.route('/page_2')
+def page_2():
+	return render_template("random_page_2.html")
+
+@app.route('/hello')
+def hello():
+	return render_template('hello.html')
+
+@app.route('/view_database')
+def view_db():
+	conn = psycopg2.connect(DATABASE_URL)
+	db = conn.cursor()
+	data = db.execute("SELECT * FROM example")
+	return render_template('view_database.html', data=data)
+
