5a504401bbc64db776ec449806c3c73a125eb534
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-07-31 22:24:07 +0100

    view_db() chage syntax, used try method

--- flask-sqlalchemy-test-02/app.py
+++ flask-sqlalchemy-test-02/app.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, render_template, g, url_for
+from flask import Flask, render_template, g, url_for, redirect
 
 
 app = Flask(__name__)
@@ -23,10 +23,23 @@ def hello():
 
 @app.route('/view_database')
 def view_db():
-	conn = psycopg2.connect(DATABASE_URL)
-	db = conn.cursor()
-	data = db.execute("SELECT * FROM example").fetachall()
-	db.close()
-	conn.close()
-	return render_template('view_database.html', data=data)
+	try:
+	    connection = psycopg2.connect(user = "fikwczdiymxhwf",
+	                                  password = "73bf42c2c8a15fa59b77e93654b6383e1cf4f85bdf0156818d1cf39a77815f13",
+	                                  host = "ec2-54-243-47-196.compute-1.amazonaws.com",
+	                                  port = "5432",
+	                                  database = "d3uburco4fea1b")
+    	db = connection.cursor()
+		data = db.execute("SELECT * FROM example").fetachall()
+		return render_template('view_database.html', data=data)
+
+    except (Exception, psycopg2.Error) as error :
+	    return "Error while connecting to PostgreSQL", error
+
+    finally:
+    #closing database connection.
+        if(connection):
+            db.close()
+            connection.close()
+            return redirect(url_for('view_db'))
 
