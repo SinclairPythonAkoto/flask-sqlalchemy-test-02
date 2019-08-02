107711c1b78d08031f92efd9ccc7264577258d43
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-02 07:24:38 +0100

    database connection syntax changed in app.py

--- flask-sqlalchemy-test-02/app.py
+++ flask-sqlalchemy-test-02/app.py
@@ -25,7 +25,9 @@ def hello():
 def view_db():
 	conn = psycopg2.connect(DATABASE_URL)
 	db = conn.cursor()
-	data = db.execute("SELECT * FROM example").fetachall()
+	query = "SELECT * FROM example WHERE name=%s"
+	db.execute(query, (name,))
+	data = db.fetachall()
 	db.close()
 	conn.close()
 	return render_template('view_database.html', data=data)
