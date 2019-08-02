77aea4fe17dfe93ee1589a7bf97de3c6f9ccee56
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-02 22:21:47 +0100

    file chnage with different syntax

--- flask-sqlalchemy-test-02/app.py
+++ flask-sqlalchemy-test-02/app.py
@@ -4,21 +4,30 @@ from flask import Flask, render_template, g, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+#from flask_heroku import Heroku
+from flak_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-
-#engine = create_engine(os.getenv("postgres://fikwczdiymxhwf:73bf42c2c8a15fa59b77e93654b6383e1cf4f85bdf0156818d1cf39a77815f13@ec2-54-243-47-196.compute-1.amazonaws.com:5432/d3uburco4fea1b"))
-#db = scoped_session(sessionmaker(bind=engine))
-
-# app.config['PROPAGATE_EXCEPTIONS'] = False
-# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = False
-# #app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
-
-# #this works 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fikwczdiymxhwf:73bf42c2c8a15fa59b77e93654b6383e1cf4f85bdf0156818d1cf39a77815f13@ec2-54-243-47-196.compute-1.amazonaws.com:5432/d3uburco4fea1b'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-DATABASE_URL = os.environ.get('DATABASE_URL', '')
-#db = SQLAlchemy(app)
+db = SQLAlchemy()
+db.init_app(app)
+
+# heroku = Heroku(app)
+# db = SQLAlchemy(app)
+
+class  Example(db.Model):
+	__tablename__ = "flights"
+	id = db.Column(db.Integer, primary_key=True)
+	info = db.Column(db.String, )
+	name = db.Column(db.String, )
+	city = db.Column(db.String, )
+
+
+
+#DATABASE_URL = os.environ.get('DATABASE_URL', '')
 
 
 @app.route('/')
@@ -35,10 +44,6 @@ def hello():
 
 @app.route('/view_database')
 def view_db():
-	conn = psycopg2.connect(DATABASE_URL)
-	cur = conn.cursor()
-	data = db.execute("SELECT * FROM example").fetachall()
-	db.close()
-	conn.close()
+	data = Example.query.filter_by(name='%s').all()
 	return render_template('view_database.html', data=data)
 
