069d5f3a4a47c401e29e3719874df7a428b774e2
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-02 18:42:04 +0100

    file change, used the SQL CS50 walk-through

--- flask-sqlalchemy-test-02/app.py
+++ flask-sqlalchemy-test-02/app.py
@@ -1,18 +1,23 @@
 import os
 import psycopg2
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, g, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 app = Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS'] = False
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = False
-#app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
 
-#this works 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fikwczdiymxhwf:73bf42c2c8a15fa59b77e93654b6383e1cf4f85bdf0156818d1cf39a77815f13@ec2-54-243-47-196.compute-1.amazonaws.com:5432/d3uburco4fea1b'
+engine = create_engine(os.getenv("postgres://fikwczdiymxhwf:73bf42c2c8a15fa59b77e93654b6383e1cf4f85bdf0156818d1cf39a77815f13@ec2-54-243-47-196.compute-1.amazonaws.com:5432/d3uburco4fea1b"))
+db = scoped_session(sessionmaker(bind=engine))
 
-DATABASE_URL = os.environ.get('DATABASE_URL', '')
+# app.config['PROPAGATE_EXCEPTIONS'] = False
+# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = False
+# #app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
+
+# #this works 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fikwczdiymxhwf:73bf42c2c8a15fa59b77e93654b6383e1cf4f85bdf0156818d1cf39a77815f13@ec2-54-243-47-196.compute-1.amazonaws.com:5432/d3uburco4fea1b'
+
+# DATABASE_URL = os.environ.get('DATABASE_URL', '')
 #db = SQLAlchemy(app)
 
 
@@ -30,10 +35,10 @@ def hello():
 
 @app.route('/view_database')
 def view_db():
-	conn = psycopg2.connect(DATABASE_URL)
-	cur = conn.cursor()
-	data = cur.execute("SELECT * FROM example").fetachall()
-	cur.close()
-	conn.close()
+	# conn = psycopg2.connect(DATABASE_URL)
+	# cur = conn.cursor()
+	data = db.execute("SELECT * FROM example").fetachall()
+	# db.close()
+	# conn.close()
 	return render_template('view_database.html', data=data)
 
